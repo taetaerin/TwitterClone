0fc5dc109fc5a4ad1b710cea7b890df8e62be309
taetaerin <taerin13@naver.com>
2023-09-12 00:12:31 +0900

    PostTweetFrom

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,15 @@
-import { auth } from "./firebase"
+import { styled } from "styled-components"
+import PostTweetFrom from "../components/post-tweet-form"
+
+const Wrapper = styled.div``;
+
+
+
 
 export default function Home() {
-    const logOut = () => {
-        auth.signOut();
-    }
     return (
-        <h1>
-            <button onClick={logOut}>Log Out</button>
-        </h1>
+        <Wrapper>
+            <PostTweetFrom />
+        </Wrapper>
     )
 }
